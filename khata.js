f612ee6367044d00978c4fa981dbd90063799373
shuvam321 <kshuvam360@gmail.com>
2022-08-17 18:58:14 +0530

    ID given

--- khata.js
+++ khata.js
@@ -1,32 +1,42 @@
-function addCustomer(){
+function addCustomer() {
     let customerdetail = document.getElementById("customerName").value;
     console.log(customerdetail);
-    if(customerdetail){
+
+
+    if (customerdetail) {
+        let customerDetail = document.createElement('div'); 
         let a1 = 1;
         let b1 = 100;
-        let random1 = a1 + (b1-a1)*Math.random();
-        console.log(random1);
-        let mainBody = document.getElementById("container");
-    let detail = document.createElement('div');
-    detail.classList.add('detail');
-    let customerDetail = document.createElement('div');
-    customerDetail.innerHTML = customerdetail;
-    customerDetail.setAttribute('id',Math.round(random1));
-    customerDetail.classList.add('customerdetailCss')
-    let youGave = document.createElement('button');
-    youGave.classList.add('youGaveCss');
-    let youGot = document.createElement('button');
-    youGot.classList.add('youGotCss');
-    youGave.innerHTML = "YOU GAVE";
-    youGot.innerHTML = "YOU GOT";
+        let random1 = a1 + (b1 - a1) * Math.random();
+        // console.log(random1);
+        var r = Math.round(random1);
+        customerDetail.setAttribute('id', r);
+        console.log(r);
+        if (r) {
+            let mainBody = document.getElementById("container");
+            let detail = document.createElement('div');
+            detail.classList.add('detail');
+            let customerDetail = document.createElement('div');
+            customerDetail.innerHTML = customerdetail;
+            customerDetail.classList.add('customerdetailCss')
+            let youGave = document.createElement('button');
+            youGave.classList.add('youGaveCss');
+            let youGot = document.createElement('button');
+            youGot.classList.add('youGotCss');
+            youGave.innerHTML = "YOU GAVE";
+            youGot.innerHTML = "YOU GOT";
 
-    detail.append(customerDetail);
-    detail.appendChild(youGave);
-    detail.appendChild(youGot);
+            detail.append(customerDetail);
+            detail.appendChild(youGave);
+            detail.appendChild(youGot);
 
-    mainBody.appendChild(detail);
+            mainBody.appendChild(detail);
+        }
+        else{
+            window.alert("Please Don't Enter same name ")
+        }
     }
-    else{
+    else {
         window.alert("Enter the Name");
     }
 }
