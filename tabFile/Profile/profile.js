5016f5566b86510b15d53a87a35f52cbaead7d63
shuvam321 <kshuvam360@gmail.com>
2022-11-18 22:16:28 +0530

    profile done

--- tabFile/Profile/profile.js
+++ tabFile/Profile/profile.js
@@ -1,8 +1,9 @@
 // console.log(localStorage.getItem('dataArray', JSON.stringify(dataArray)));
 
 // localStorage.getItem('profileData', JSON.stringify(loginDetail));
-var data = localStorage.getItem('profileData');
-console.log(localStorage.getItem('profileData'));
+var data = JSON.parse(localStorage.getItem('profileData'));
+console.log(typeof(data));
+console.log(localStorage.getItem('profileData'))
 // console.log(localStorage.getItem('dataArray'));
     // console.log(person)
 //     var detail = localStorage.getItem('dataArray')
@@ -20,7 +21,24 @@ console.log(localStorage.getItem('profileData'));
     
    
 // }
-        console.log(document.getElementById("usernameDetail").innerHTML =data.user);
-        document.getElementById("emailDetail").innerHTML =data.email; 
-        document.getElementById("usernameDetail").innerHTML =data.user;       
+        console.log(document.getElementById("usernameDetail").innerHTML = data.userName);
+        document.getElementById("emailDetail").innerHTML = data.email; 
+        document.getElementById("usernameDetail").innerHTML = data.userName;       
+//upload jquery
 
+$(document).ready(function(){
+    // Prepare the preview for profile picture
+        $("#wizard-picture").change(function(){
+            readURL(this);
+        });
+    });
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+    
+            reader.onload = function (e) {
+                $('#wizardPicturePreview').attr('src', e.target.result).fadeIn('slow');
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
